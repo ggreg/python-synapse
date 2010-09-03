1c30a8d02120a7b30e40691c73b583a3b0bfd80e
Greg Leclercq <ggl@0x80.net>
2010-09-03 15:51:45 +0200

    update setup.py, add dependency to pyzmq

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='synapse',
-      version='0.1',
-      description='Distributed communication module',
-      author='Greg Leclercq',
-      author_email='greg@0x80.net',
-      url='http://github.org/ggreg/python-synapse',
-      packages=['synapse']
+      version       = '0.1',
+      description   = 'Distributed communication module',
+      author        = 'Greg Leclercq',
+      author_email  = 'greg@0x80.net',
+      url           = 'http://github.org/ggreg/python-synapse',
+      packages      = ['synapse'],
+      requires      = ['pyzmq (>= 2.0.7)']
      )
